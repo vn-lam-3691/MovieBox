9383d59ae6f930fcf0994b7eb4ad387f133b4a61
VanLamTruong <lamtruong3242@gmail.com>
2024-03-28 10:10:51 +0700

    init project

---
+++ app/src/main/java/com/vanlam/moviebox/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.vanlam.moviebox
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vanlam.moviebox.ui.theme.MovieBoxTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MovieBoxTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    
+                }
+            }
+        }
+    }
+}
